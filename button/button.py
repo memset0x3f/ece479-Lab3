46762e83ca47974328b71ce9ea7cbab9a3ec7b4b
IILKA <fanz1@connect.hku.hk>
2025-04-28 01:25:57 -0500

    button transmission OK

--- button/button.py
+++ button/button.py
@@ -2,18 +2,20 @@ import RPi.GPIO as GPIO
 import time
 from enum import Enum
 
-class KeyState(Enum):
-    PRESSED = 0
-    RELEASED = 1
-    OnClick = 2
-    OnRelease = 3
+class KeyState():
+    PRESSED = "pressed"
+    RELEASED = "released"
+    OnClick = "onclick"
+    OnRelease = "onrelease"
 
 class ButtonDetector:
     def __init__(self, buttons_addr: list):
         self.n_keys = len(buttons_addr)
         self.button_addr = buttons_addr
         self.addr2button = {x:i for i, x in enumerate(buttons_addr)}
-        self.key_states = {x:KeyState.RELEASED for x in buttons_addr}
+        self.KeyState = KeyState()
+        print(self.KeyState)
+        self.key_states = {x:self.KeyState.RELEASED for x in buttons_addr}
 
         GPIO.setmode(GPIO.BCM)
         for key in buttons_addr:
@@ -22,18 +24,18 @@ class ButtonDetector:
 
     def detectKey(self, key):
         if GPIO.input(key) == 0:
-            if(self.key_states[key] == KeyState.RELEASED):
+            if(self.key_states[key] == self.KeyState.RELEASED):
                 # print(f"KEY {self.addr2button[key]} PRESS") 
-                self.key_states[key] = KeyState.OnClick
+                self.key_states[key] = self.KeyState.OnClick
             else:
-                self.key_states[key] = KeyState.PRESSED
+                self.key_states[key] = self.KeyState.PRESSED
             # self.key_states[key] = KeyState.PRESSED
         else:
-            if(self.key_states[key] == KeyState.PRESSED):
+            if(self.key_states[key] == self.KeyState.PRESSED):
                 # print(f"KEY {self.addr2button[key]} RELEASE") 
-                self.key_states[key] = KeyState.OnRelease
+                self.key_states[key] = self.KeyState.OnRelease
             else:
-                self.key_states[key] = KeyState.RELEASED
+                self.key_states[key] = self.KeyState.RELEASED
         
         
             # self.key_states[key] = KeyState.RELEASED
@@ -46,7 +48,10 @@ class ButtonDetector:
     def detect(self):                    
         while True:                     
             time.sleep(0.05)
-            print(self.key_states)
+            # print(self.key_states)
             for key in self.button_addr:
                 self.detectKey(key)
 
+
+
+    
