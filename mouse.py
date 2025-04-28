9d704d5b81cb795a9b8b091f2034091e42f56cdc
IILKA <fanz1@connect.hku.hk>
2025-04-29 03:41:49 +0800

    sync

--- mouse.py
+++ mouse.py
@@ -51,48 +51,48 @@ class PC_Controller:
     def press(self, key):
         pyautogui.press(key)
 
-if __name__ == "__main__":
-    print("Press Ctrl+C to exit")
-    mouse = PC_Controller()
-    while True: 
-        x = int(input("x: "))
-        y = int(input("y: "))
-        x, y = mouse.solve(x, y)
-        print(f"solved x: {x}, y: {y}")
-        mouse.move_to(x, y)
+# if __name__ == "__main__":
+#     print("Press Ctrl+C to exit")
+#     mouse = PC_Controller()
+#     while True: 
+#         x = int(input("x: "))
+#         y = int(input("y: "))
+#         x, y = mouse.solve(x, y)
+#         print(f"solved x: {x}, y: {y}")
+#         mouse.move_to(x, y)
     # mouse.click(x, y)
 #make a [(x,y)] of circle in the center of the screen,
 #with radius 100
 #input (x0, y0) is the center of the circle
 #and num of points 
-# def circle(x0, y0, radius, num_points=72):
-#     points = []
-#     for i in range(num_points):
-#         angle = 2 * math.pi * i / num_points
-#         x = int(x0 + radius * math.cos(angle))
-#         y = int(y0 + radius * math.sin(angle))
-#         points.append((x, y))
-#     return points
+def circle(x0, y0, radius, num_points=72):
+    points = []
+    for i in range(num_points):
+        angle = 2 * math.pi * i / num_points
+        x = int(x0 + radius * math.cos(angle))
+        y = int(y0 + radius * math.sin(angle))
+        points.append((x, y))
+    return points
 
 
-# mouse = Mouse()
-# circle = circle(mouse.center_x, mouse.center_y, 200, 360)
-# print(len(circle))
-# cnt = 0
-# # pyautogui.PAUSE = 0.0001
-# while True: 
+mouse = PC_Controller()
+circle = circle(mouse.center_x, mouse.center_y, 200, 36)
+print(len(circle))
+cnt = 0
+# pyautogui.PAUSE = 0.0001
+while True: 
     
-#     start_time = time.time()
-#     x, y = circle[cnt]
-#     mouse.move_to(x, y)
-#     cnt += 1
-#     if cnt >= len(circle):
-#         cnt = 0
-#     end_time = time.time()
-#     print(f"Moved to ({x}, {y}) in {end_time - start_time:.6f} seconds")
-    # if cnt == 0:
-        # mouse.click(x, y)
-    # time.sleep(0.0001)
+    start_time = time.time()
+    x, y = circle[cnt]
+    mouse.move_to(x, y)
+    cnt += 1
+    if cnt >= len(circle):
+        cnt = 0
+    end_time = time.time()
+    print(f"Moved to ({x}, {y}) in {end_time - start_time:.6f} seconds")
+    if cnt == 0:
+        mouse.click(x, y)
+    time.sleep(1)
 
     
 
